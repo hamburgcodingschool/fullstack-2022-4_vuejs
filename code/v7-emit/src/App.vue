c8ea3d96d826d5118c8a71017c9c5351e470a4a5
Helder Pereira <phelder@gmail.com>
2023-02-24 18:56:10 +0100

    lesson i dunno

---
+++ code/v7-emit/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Hello Vue!</h1>
+  <CustomButton text="My First Button" @myClick="button1ClickHandler" />
+  <CustomButton text="My Second Button" @myClick="button2ClickHandler" />
+  <CustomButton text="My Last Button" @myClick="button3ClickHandler" />
+</template>
+
+<script>
+import CustomButton from "./components/CustomButton.vue";
+
+export default {
+  components: {
+    CustomButton
+  },
+  methods: {
+    button1ClickHandler(value) {
+      console.log(value);
+      // alert(event);
+    },
+    button2ClickHandler() {
+      alert("Codisfgjoidfsjgpodfjgpodfjpgodj")
+    },
+    button3ClickHandler() {
+      alert("🤣")
+    }
+  }
+}
+</script>
---
+++ code/v7-emit/src/components/CustomButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button @click="clickHandler">{{ text }}</button>
+</template>
+
+<script>
+
+
+export default {
+    props: {
+        text: String
+    },
+    emits: ["myClick"],
+    methods: {
+        clickHandler(event) {
+            this.$emit("myClick", event);
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    border: solid 3px black;
+    background-color: aquamarine;
+    border-radius: 20px;
+    padding: 5px 10px;
+    box-shadow: 10px 10px 10px grey;
+    cursor: pointer;
+}
+
+button:hover {
+    box-shadow: none;
+}
+</style>
